ddccc00f17cd0cbf6cc3c67357353982a08f127f
Bbest1365 <best1365@gmail.com>
2024-09-10 23:41:59 +0700

    do_while_proj3

---
+++ do_while_proj2/Program.cs
@@ -0,0 +1,28 @@
+﻿string? useroriginal;
+string? userconv;
+
+do
+{
+    Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+    useroriginal = Console.ReadLine().Trim();
+    userconv = useroriginal.ToLower();
+    if (userconv == "administrator")
+    {
+        Console.WriteLine($"Your input value ({useroriginal}) has been accepted.");
+        break;
+    }
+
+    else if (userconv == "manager")
+    {
+        Console.WriteLine($"Your input value ({useroriginal}) has been accepted.");
+        break;
+    }
+    else if (userconv == "user")
+    {
+        Console.WriteLine($"Your input value ({useroriginal}) has been accepted.");
+        break;
+    }
+    else
+        Console.WriteLine($"The role name that you entered, \"{useroriginal}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+
+} while (true);
