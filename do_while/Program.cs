669e5ceea5c1fd258f2fa650bcb9185ec5996d4b
Bbest1365 <best1365@gmail.com>
2024-09-10 21:24:09 +0700

    do_while_strings

--- do_while/Program.cs
+++ do_while/Program.cs
@@ -1,21 +1,23 @@
-﻿int hero = 10;
-int monster = 10;
+﻿string? readResult;
+bool validEntry = false;
+// capture user input in a string variable named readResult
 
-Random dice = new Random();
-
+int numericValue = 0;
+bool validNumber = false;
+Console.WriteLine("Enter a string containing at least three characters:");
 do
 {
-    int roll = dice.Next(1, 11);
-    monster -= roll;
-    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
-
-    if (monster <= 0) continue;
-
-    roll = dice.Next(1, 11);
-    hero -= roll;
-    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
-
-} while (hero > 0 && monster > 0);
-
-Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
-
+    readResult = Console.ReadLine();
+    validNumber = int.TryParse(readResult, out numericValue);
+    if (readResult != null)
+    {
+        if (readResult.Length >= 3)
+        {
+            validEntry = true;
+        }
+        else
+        {
+            Console.WriteLine("Your input is invalid, please try again.");
+        }
+    }
+} while (validEntry == false);
