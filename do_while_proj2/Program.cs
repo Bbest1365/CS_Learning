8e543c1cf52e45eb06e05cdd96539b79616313b2
Bbest1365 <best1365@gmail.com>
2024-09-11 00:20:37 +0700

    update solution

--- do_while_proj2/Program.cs
+++ do_while_proj2/Program.cs
@@ -26,3 +26,33 @@ do
         Console.WriteLine($"The role name that you entered, \"{useroriginal}\" is not valid. Enter your role name (Administrator, Manager, or User)");
 
 } while (true);
+
+//This is Solution
+/*
+string? readResult;
+string roleName = "";
+bool validEntry = false;
+
+do
+{                
+    Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+    readResult = Console.ReadLine();
+    if (readResult != null) 
+    {
+        roleName = readResult.Trim();
+    }
+
+    if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+    {
+        validEntry = true;
+    }
+    else
+    {
+        Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+    }
+
+} while (validEntry == false);
+
+Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+readResult = Console.ReadLine();
+*/
