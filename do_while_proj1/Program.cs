af554421fb0496f671e085a01ce3d9c038d717b9
Bbest1365 <best1365@gmail.com>
2024-09-11 00:20:23 +0700

    update solution

--- do_while_proj1/Program.cs
+++ do_while_proj1/Program.cs
@@ -17,3 +17,41 @@ do
         Console.WriteLine($"You entered {numericValue} Please enter a number between 5 and 10.");
 
 } while (true);
+
+//This is solution
+
+/*string? readResult;
+string valueEntered = "";
+int numValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null) 
+    {
+        valueEntered = readResult;
+    }
+
+    validNumber = int.TryParse(valueEntered, out numValue);
+
+    if (validNumber == true)
+    {
+        if (numValue <= 5 || numValue >= 10)
+        {
+            validNumber = false;
+            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+} while (validNumber == false);
+
+Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+readResult = Console.ReadLine();
+*/
