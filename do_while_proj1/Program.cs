08187609e954ae3a97c3fc905b7b534ad2fa7b14
Bbest1365 <best1365@gmail.com>
2024-09-10 22:12:30 +0700

    do_while proj1

---
+++ do_while_proj1/Program.cs
@@ -0,0 +1,19 @@
+﻿string? readResult;
+
+int numericValue = 0;
+bool validNumber = false;
+Console.WriteLine("Enter an integer value between 5 and 10");
+do
+{
+    readResult = Console.ReadLine();
+    validNumber = int.TryParse(readResult, out numericValue);
+    // Console.WriteLine(validNumber);
+    // Console.WriteLine(numericValue);
+    if (numericValue > 5 && numericValue < 10)
+        Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+    else if (numericValue == 0)
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    else
+        Console.WriteLine($"You entered {numericValue} Please enter a number between 5 and 10.");
+
+} while (true);
