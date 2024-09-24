a0d27b4ba06a7df8c8d0181739b772978393ca91
Bbest1365 <best1365@gmail.com>
2024-09-24 21:04:45 +0700

    update clear && Resize

--- sort_reverse/Program.cs
+++ sort_reverse/Program.cs
@@ -1,16 +1,28 @@
-﻿string[] pallets = { "B14", "A11", "B12", "A13" };
+﻿
+// Array.Sort(); and Array.Reverse();
+// string[] pallets = { "B14", "A11", "B12", "A13" };
 
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
 
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+// Array.Clear(); and Array.Resize();
+string[] pallets = { "B14", "A11", "B12", "A13" };
 Console.WriteLine("");
-Console.WriteLine("Reversed...");
-Array.Reverse(pallets);
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
 foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
-}
+} 
