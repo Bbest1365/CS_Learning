56beabdc49b4ccfa431f4f1d6bfe8613da9bb94a
Bbest1365 <best1365@gmail.com>
2024-09-26 00:20:06 +0700

    update

--- sort_reverse/Program.cs
+++ sort_reverse/Program.cs
@@ -57,22 +57,24 @@
 // Console.WriteLine(result);
 
 
-string pangram = "The quick brown fox jumps over the lazy dog";
+// string pangram = "The quick brown fox jumps over the lazy dog";
 
-// Step 1
-string[] message = pangram.Split(' ');
+// // Step 1
+// string[] message = pangram.Split(' ');
 
-//Step 2
-string[] newMessage = new string[message.Length];
+// //Step 2
+// string[] newMessage = new string[message.Length];
 
-// Step 3
-for (int i = 0; i < message.Length; i++)
-{
-    char[] letters = message[i].ToCharArray();
-    Array.Reverse(letters);
-    newMessage[i] = new string(letters);
-}
+// // Step 3
+// for (int i = 0; i < message.Length; i++)
+// {
+//     char[] letters = message[i].ToCharArray();
+//     Array.Reverse(letters);
+//     newMessage[i] = new string(letters);
+// }
 
-//Step 4
-string result = String.Join(" ", newMessage);
-Console.WriteLine(result);
+// //Step 4
+// string result = String.Join(" ", newMessage);
+// Console.WriteLine(result);
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
