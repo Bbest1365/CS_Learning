0067d2a181ed327276fa94c8129cd98ce9bd289a
Bbest1365 <best1365@gmail.com>
2024-10-03 23:29:29 +0700

    update

--- composit_formatting/Program.cs
+++ composit_formatting/Program.cs
@@ -70,11 +70,12 @@ string input = "Pad this";  //Padleft
  Console.WriteLine(input.PadRight(12, '-'));
 
 string paymentId = "769C";
-string payeeName = "Mr. Stephen Ortega"; 
-string paymentAmount = "$5,000.00";
+ string payeeName = "Mr. Stephen Ortega";
+ string paymentAmount = "$5,000.00";
 
-var formattedLine = paymentId.PadRight(6);
-formattedLine += payeeName.PadRight(24);
-formattedLine += paymentAmount.PadLeft(10);
+ var formattedLine = paymentId.PadRight(6);
+ formattedLine += payeeName.PadRight(24);
+ formattedLine += paymentAmount.PadLeft(10);
 
-Console.WriteLine(formattedLine);
+ Console.WriteLine("1234567890123456789012345678901234567890");
+ Console.WriteLine(formattedLine);
